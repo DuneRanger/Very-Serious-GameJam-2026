8f5c60f81939171e55b8fc8f8ccaf2680435a2c5
IgorBurger <muller.martin.mm@gmail.com>
2026-06-20 18:57:53 +0200

    Modified boost bar (again)

--- scenes/game/game_scene/boost_bar/boost_bar.gd
+++ scenes/game/game_scene/boost_bar/boost_bar.gd
@@ -3,6 +3,6 @@ extends Node2D
 var boost_modifier
 @onready var boost_indicator: ColorRect = $bar/outer/inner/boost_indicator
 
-func _on_boost_bar_button_pressed() -> void:
+func _on_button_pressed() -> void:
 	boost_modifier = boost_indicator.scale.y
 	print(boost_modifier)
