0dee8cf4cce7bbc26d30fba72f932c5b61ec36fa
IgorBurger <muller.martin.mm@gmail.com>
2026-06-20 21:41:54 +0200

    Remade booster bar (again)
    now with an updated texture

--- scenes/game/game_scene/boost_bar/boost_bar.gd
+++ scenes/game/game_scene/boost_bar/boost_bar.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var boost_modifier
-@onready var boost_indicator: ColorRect = $bar/outer/inner/boost_indicator
+@onready var boost_indicator: ColorRect = $bar/inner/boost_indicator
 
 func _on_button_pressed() -> void:
 	boost_modifier = boost_indicator.scale.y
