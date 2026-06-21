3c2a25c391e60b806689d9eb703d44d5f8e007a5
Topeni <matysekz@centrum.cz>
2026-06-21 13:29:30 +0200

    removed whitespaces

--- scenes/game/game_scene/betting_system/bet_button.gd
+++ scenes/game/game_scene/betting_system/bet_button.gd
@@ -13,11 +13,8 @@ func init(sizex: float, sizey: float, posx: float, posy: float, id: int) -> void
 	position.x = posx
 	position.y = posy
 	button_id = id
-	
 	amount_bet = 0;
-	
 	to_process = false
-
 
 func _on_button_down() -> void:
 	to_process = true
