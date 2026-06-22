d1ffe98fcbeef118db6546d642d1cf812834e05b
Topeni <matysekz@centrum.cz>
2026-06-22 15:00:15 +0200

    removed unnecessary ready and process functions in game manager

--- scenes/game/game_manager/game_manager.gd
+++ scenes/game/game_manager/game_manager.gd
@@ -1,19 +1,8 @@
 extends Node2D
 
-
 var round_budget: int
 var round_count: int
 var money: int
 var tokens: int
 var spins: int
 var boosts: int
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
