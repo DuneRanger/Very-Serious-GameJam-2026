bd6257a4aa223c7b04f1a092092fb0e569e7c7d8
Topeni <matysekz@centrum.cz>
2026-06-22 23:05:03 +0200

    made the error animation stop (final fix to the error animation i swear)

--- scenes/game/game_scene/game_scene.gd
+++ scenes/game/game_scene/game_scene.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if $Table/BettingSystem.error_message != "":
 		send_error_message($Table/BettingSystem.error_message)
+		$Table/BettingSystem.error_message = ""
 		
 	match (GameManager.game_state):
 		GameEnums.game_states.BET_PHASE:
