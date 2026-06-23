40269034a114b6bad1b72009febfdb0530373b2a
IgorBurger <muller.martin.mm@gmail.com>
2026-06-23 21:33:48 +0200

    Renamed a few animation functions

--- scenes/game/UI_elements/animated_sprites/boost_symbol/boost_symbol.gd
+++ scenes/game/UI_elements/animated_sprites/boost_symbol/boost_symbol.gd
@@ -2,31 +2,30 @@ extends Node2D
 
 @onready var animation_player: AnimationPlayer = $Control/AnimationPlayer
 @onready var boost_used_sprite: Sprite2D = $Control/BoostUsedSprite
-@onready var animated_sprite_2d: AnimatedSprite2D = $Control/RefilAnimation/AnimatedSprite2D
 @onready var boost_sprite: Sprite2D = $Control/BoostSprite
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	boost_idle()
+	boost_idle_anim()
 
-func boost_idle():
+func boost_idle_anim():
 	animation_player.play("boost_idle")
 	var randomTime = randf_range(0, 4)
 	animation_player.seek(randomTime)
 
-func refil_boost():
+func refil_boost_anim():
 	animation_player.play("RefilBoost")
 
 	
-func use_boost():
+func use_boost_anim():
 	animation_player.play("UseBoost")
 
 
 
 func _on_test_button_pressed() -> void:
-	refil_boost()
+	refil_boost_anim()
 
 
 func _on_test_button_2_pressed() -> void:
-	use_boost()
+	use_boost_anim()
