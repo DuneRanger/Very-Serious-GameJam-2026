237b762792358bfb4ea76f02c11e29658a0ec2b0
DuneRanger <daniel.culliver@gmail.com>
2026-06-23 22:55:46 +0200

    start of three-collider bank system

--- scenes/game/game_scene/roulette/roulette_bank.gd
+++ scenes/game/game_scene/roulette/roulette_bank.gd
@@ -11,7 +11,9 @@ var top_edge_radius : float
 var bottom_edge_radius : float
 
 var polygon_points : PackedVector2Array = []
-var bank_collider : CollisionShape2D
+var outer_collider : CollisionShape2D
+var left_collider : CollisionShape2D
+var right_collider : CollisionShape2D
 
 func get_bank_position():
 	return global_position + local_bank_position
@@ -25,16 +27,40 @@ func get_arc_points(center, radius, angle_from, angle_to) -> PackedVector2Array:
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
 	return points_arc
 
-func build_bank_polygon(bank_angle: float, radius_center: float, width: float) -> PackedVector2Array:
+func build_colliders(bank_angle: float, radius_center: float, width: float):
 	var outer_r = radius_center + width * 0.5
 	var inner_r = radius_center - width * 0.5
+	var left_angle = -bank_angle * 0.5
+	var right_angle = bank_angle * 0.5
 
-	var pts = PackedVector2Array()
+	var outer_convex = Line2D.new()
+	outer_convex.points = get_arc_points(Vector2.ZERO, outer_r, left_angle, right_angle)
+	match cell.colour:
+		Color.BLACK:
+			outer_convex.default_color = Color(0.9, 0.9, 0.9, 0.2)
+		_:
+			outer_convex.default_color = Color(0.1, 0.1, 0.1, 0.4)
+	add_child(outer_convex)
+	outer_collider = CollisionShape2D.new()
+	outer_collider.shape = outer_convex
+	add_child(outer_collider)
+	
+	var left_side = Line2D.new()
+	left_side.points = [Vector2(cos(left_angle), sin(left_angle)) * inner_r, Vector2(cos(left_angle), sin(left_angle)) * outer_r]
+	left_side.default_color = Color.SILVER
+	add_child(left_side)
+	left_collider = CollisionShape2D.new()
+	left_collider.shape = left_side
+	add_child(left_collider)
+	
+	var right_side = Line2D.new()
+	right_side.points = [Vector2(cos(right_angle), sin(right_angle)) * inner_r, Vector2(cos(right_angle), sin(right_angle)) * outer_r]
+	right_side.default_color = Color.SILVER
+	add_child(right_side)
+	right_collider = CollisionShape2D.new()
+	right_collider.shape = right_side
+	add_child(right_collider)
 
-	pts.append_array(get_arc_points(Vector2.ZERO, outer_r, -bank_angle * 0.5, bank_angle * 0.5))
-	pts.append_array(get_arc_points(Vector2.ZERO, inner_r, bank_angle * 0.5, -bank_angle * 0.5))
-
-	return pts
 
 func _init(owner_cell: RouletteCell, bank_angle: float, bank_position: Vector2 = Vector2.ZERO, bank_width: float = 45.0):
 	cell = owner_cell
@@ -48,24 +74,25 @@ func _init(owner_cell: RouletteCell, bank_angle: float, bank_position: Vector2 =
 		position_visual.position = local_bank_position
 		position_visual.polygon = RouletteBall._make_circle_points(20, 24)
 		add_child(position_visual)
-
-	polygon_points = build_bank_polygon(bank_angle - 0.05, bank_position.length(), bank_width)
+	
+	build_colliders(bank_angle, bank_position.length() * 0.985, bank_width)
+	#polygon_points = build_bank_polygon(bank_angle - 0.05, bank_position.length(), bank_width)
 	
 	var convex = ConvexPolygonShape2D.new()
 	convex.points = polygon_points
 
-	bank_collider = CollisionShape2D.new()
-	bank_collider.shape = convex
-	add_child(bank_collider)
+	#bank_collider = CollisionShape2D.new()
+	#bank_collider.shape = convex
+	#add_child(bank_collider)
 	
-	var visual = Polygon2D.new()
-	match cell.colour:
-		Color.BLACK:
-			visual.color = Color(0.9, 0.9, 0.9, 0.2)
-		_:
-			visual.color = Color(0.1, 0.1, 0.1, 0.2)
-	visual.polygon = polygon_points
-	add_child(visual)
+	#var visual = Polygon2D.new()
+	#match cell.colour:
+		#Color.BLACK:
+			#visual.color = Color(0.9, 0.9, 0.9, 0.2)
+		#_:
+			#visual.color = Color(0.1, 0.1, 0.1, 0.2)
+	#visual.polygon = polygon_points
+	#add_child(visual)
 
 	body_entered.connect(_on_body_entered)
 
