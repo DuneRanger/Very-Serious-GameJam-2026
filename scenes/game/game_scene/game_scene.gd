33dfc6291a691fedc53ff5b802c9a035be2872b0
Topeni <matysekz@centrum.cz>
2026-06-23 14:30:56 +0200

    made gamemanager global and autoloaded

--- scenes/game/game_scene/game_scene.gd
+++ scenes/game/game_scene/game_scene.gd
@@ -3,28 +3,27 @@ extends Node2D
 class_name GameScene
 
 func _ready() -> void:
-	GameManager.money = 100
-	GameManager.game_state = GameEnums.game_states.BET_PHASE
+	GameManagerGlobal.game_state = GameEnums.game_states.BET_PHASE
 	$Table/BettingSystem.send_error_message.connect(send_error_message)
 
 func _process(_delta: float) -> void:
-	match (GameManager.game_state):
+	match (GameManagerGlobal.game_state):
 		GameEnums.game_states.BET_PHASE:
 			if Input.is_action_just_pressed("rotate_roullete"):
 				$Table/Roulette.spin_roulette()
-				GameManager.game_state = GameEnums.game_states.SPIN_PHASE
+				GameManagerGlobal.game_state = GameEnums.game_states.SPIN_PHASE
 		GameEnums.game_states.SPIN_PHASE:
 			if Input.is_action_just_pressed("stop_roullete"):
 				$Table/Roulette.stop_roulette()
-				GameManager.game_state = GameEnums.game_states.BET_PHASE
-				GameManager.money += get_full_bet_win()
+				GameManagerGlobal.game_state = GameEnums.game_states.BET_PHASE
+				var money_won = get_full_bet_win()
 		_:
 			pass
 
 func does_bet_win(bet_id : int) -> bool:
 	var bet_type : GameEnums.bet_types = $Table/BettingSystem.get_bet_type(bet_id)
 	if bet_type == GameEnums.bet_types.NUMBER:
-		for cell : GameManager.RouletteCell in GameManager.caughtCells:
+		for cell : GameManagerGlobal.RouletteCell in GameManagerGlobal.caughtCells:
 			if cell.number == bet_id:
 				return true
 				
@@ -39,19 +38,19 @@ func does_bet_win(bet_id : int) -> bool:
 			step = GameEnums.max_roulette_num / 2
 		var lower_bound = (shift - 1) * step + 1
 		var upper_bound = shift * step
-		for cell : GameManager.RouletteCell in GameManager.caughtCells:
+		for cell : GameManagerGlobal.RouletteCell in GameManagerGlobal.caughtCells:
 			if cell.number >= lower_bound && cell.number <= upper_bound:
 				return true
 				
 	elif bet_id == (GameEnums.max_roulette_num + 6) || bet_id == (GameEnums.max_roulette_num + 7):
 		var mod_res = bet_id - (GameEnums.max_roulette_num + 6)
-		for cell : GameManager.RouletteCell in GameManager.caughtCells:
+		for cell : GameManagerGlobal.RouletteCell in GameManagerGlobal.caughtCells:
 			if (cell.number % 2) == mod_res:
 				return true
 	elif bet_id == (GameEnums.max_roulette_num + 8) || bet_id == (GameEnums.max_roulette_num + 9):
 		var whatever = bet_id - (GameEnums.max_roulette_num + 8)
 		var color : Color = Color.BLACK if whatever else Color.RED
-		for cell : GameManager.RouletteCell in GameManager.caughtCells:
+		for cell : GameManagerGlobal.RouletteCell in GameManagerGlobal.caughtCells:
 			if cell.colour == color:
 				return true
 			
@@ -67,18 +66,18 @@ func get_bet_coeff(bet_id : int) -> float:
 	var bet_type = $Table/BettingSystem.get_bet_type(bet_id)
 	match bet_type:
 		GameEnums.bet_types.NUMBER:
-			return GameManager.single_bet_coeff
+			return GameManagerGlobal.single_bet_coeff
 		GameEnums.bet_types.THIRD:
-			return GameManager.third_bet_coeff
+			return GameManagerGlobal.third_bet_coeff
 		GameEnums.bet_types.HALF:
-			return GameManager.half_bet_coeff
+			return GameManagerGlobal.half_bet_coeff
 		_:
 			return 0.0
 
 func get_full_bet_win() -> int:
 	var value = 0
-	for bet_id in GameManager.bets:
-		var bet = GameManager.bets[bet_id]
+	for bet_id in GameManagerGlobal.bets:
+		var bet = GameManagerGlobal.bets[bet_id]
 		if does_bet_win(bet_id):
 			var new_value = int (bet * get_bet_coeff(bet_id))
 			print("Won bet: ", bet_id, ", amount won: ", new_value)
--- scenes/game/game_scene/roulette/roulette.gd
+++ scenes/game/game_scene/roulette/roulette.gd
@@ -13,18 +13,18 @@ var default_font : Font = ThemeDB.fallback_font;
 
 const base_roulette_numbers : int = 24
 # (base_roulette_numbers + 1) includes the green 0
-var total_weight : float = (base_roulette_numbers + 1) * GameManager.base_cell_weight
+var total_weight : float = (base_roulette_numbers + 1) * GameManagerGlobal.base_cell_weight
 
 enum CellMod {STICKY, SHINY}
 
 
 
 func _init():
-	cells.append(GameManager.RouletteCell.new(0, Color.GREEN))
+	cells.append(GameManagerGlobal.RouletteCell.new(0, Color.GREEN))
 	for i in range(base_roulette_numbers):
 		var curCol = Color.RED
 		if i % 2 == 0: curCol = Color.BLACK
-		cells.append(GameManager.RouletteCell.new(i + 1, curCol))
+		cells.append(GameManagerGlobal.RouletteCell.new(i + 1, curCol))
 
 # -------------------------------- Drawing --------------------------------
 
@@ -139,10 +139,10 @@ func _physics_process(_delta: float):
 	decay_rotation()
 	queue_redraw()
 	give_balls_angular_velocity(_delta)
-	match (GameManager.game_state):
+	match (GameManagerGlobal.game_state):
 		GameEnums.game_states.SPIN_PHASE:
 			if rotation_speed == 0:
-				GameManager.game_state = GameEnums.game_states.BET_PHASE
+				GameManagerGlobal.game_state = GameEnums.game_states.BET_PHASE
 		GameEnums.game_states.BET_PHASE:
 			pass
 		_:
@@ -244,7 +244,7 @@ func _on_bank_body_entered(body: RouletteBall, cell, bank_area: Area2D) -> void:
 
 	if randf() < effective_chance:
 		body.catch_in_pocket(bank_area.global_position, bank_area)
-		GameManager.caughtCells.push_back(cell)
+		GameManagerGlobal.caughtCells.push_back(cell)
 		print("Ball caught at number ", cell.number)
 	else:
 		print("Ball missed cell ", cell.number)
