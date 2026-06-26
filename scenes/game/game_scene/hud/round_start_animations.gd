5bc8d4c5e0b683459120c7e65c88975b372948e2
IgorBurger <muller.martin.mm@gmail.com>
2026-06-26 15:10:08 +0200

    added more SFX

--- scenes/game/game_scene/hud/round_start_animations.gd
+++ scenes/game/game_scene/hud/round_start_animations.gd
@@ -8,6 +8,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_round_start():
+	$SFXTimer.start()
 	$AnimationPlayer.play("RoundStart")
 	$RoundNumberLabel.text = "Round " + str(GameManagerGlobal.round_count)
 	$QuotaMessageLabel.text = "You need " + str(GameManagerGlobal.quota) + " money for " + GameManagerGlobal.current_quota_message
+
+
+
+func _on_sfx_timer_timeout() -> void:
+	$SFXTimer.stop()
+	SfxManager.play_SFX("res://assets/SFX/new_round.ogg")
