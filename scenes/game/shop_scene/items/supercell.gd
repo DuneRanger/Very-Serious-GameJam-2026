df7d7a51600c3357a599ad1a3e5a6c55d5fa8de6
Daniel Culliver <daniel.culliver@gmail.com>
2026-06-26 17:24:41 +0200

    Implement supercell item

---
+++ scenes/game/shop_scene/items/supercell.gd
@@ -0,0 +1,30 @@
+class_name supercell extends ShopItem
+
+var cost : int = 7
+var image_path : String # = "res://assets/textures/items/item_supercell.png"
+var name : String = "SuperCell"
+var target_number : int
+var description : String
+
+func get_cost() -> int:
+	return cost
+
+func get_image_path() -> String:
+	return image_path
+
+func _init() -> void:
+	target_number = GameManagerGlobal.cells.pick_random().number
+	description = "Increase the weight of a single number %d pocket by three\n" % target_number
+
+func get_name() -> String:
+	return name
+
+func get_description() -> String:
+	return description
+
+func apply_effect() -> void:
+	for cell in GameManagerGlobal.cells:
+		if cell.number == target_number:
+			cell.weight += 3
+			break
+	GameManagerGlobal.commit_cell_change.emit()
