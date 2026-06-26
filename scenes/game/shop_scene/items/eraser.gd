b9925f9bccb32b4c445c5f03204e17516fd4734b
Daniel Culliver <daniel.culliver@gmail.com>
2026-06-26 17:17:02 +0200

    Implement eraser item

---
+++ scenes/game/shop_scene/items/eraser.gd
@@ -0,0 +1,30 @@
+extends ShopItem
+
+class_name eraser
+
+var cost : int = 4
+var image_path : String = "res://assets/textures/items/item_eraser.png"
+var name : String = "Eraser"
+var description : String
+var target_cell : RouletteCell
+
+func get_cost() -> int:
+	return cost
+
+func get_image_path() -> String:
+	return image_path
+
+func _init() -> void:
+	target_cell = GameManagerGlobal.cells.pick_random()
+	while target_cell.colour == Color.DARK_GREEN: target_cell = GameManagerGlobal.cells.pick_random()
+	description = "Remove a " + str(target_cell) + " from the roulette"
+
+func get_name() -> String:
+	return name
+
+func get_description() -> String:
+	return description
+
+func apply_effect() -> void:
+	GameManagerGlobal.cells.erase(target_cell)
+	GameManagerGlobal.commit_cell_change.emit()
