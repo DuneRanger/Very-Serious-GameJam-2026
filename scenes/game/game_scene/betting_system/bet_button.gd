4565c16a0c9a1c3f569ee2411eb7926b0f6f5dd3
Topeni <matysekz@centrum.cz>
2026-07-07 19:27:03 +0200

    Changed the betting system container and added a popup for money betted on a field

--- scenes/game/game_scene/betting_system/bet_button.gd
+++ scenes/game/game_scene/betting_system/bet_button.gd
@@ -1,61 +1,75 @@
 class_name bet_button extends Button
 
-const base_color : Color = Color.GREEN
-const red_color : Color = Color.DARK_RED
-const black_color : Color = Color.BLACK
+const base_color : Color = Color("326e2dff")
+const red_color : Color = Color("d13030ff")
+const black_color : Color = Color("000000ff")
+
+var stylebox : StyleBoxFlat = null
 
 @export var button_id: int
 var amount_bet : int
 
-func set_color(new_color : Color):
-	var stylebox : StyleBoxFlat = StyleBoxFlat.new()
-	stylebox.bg_color = new_color
+func make_stylebox():
+	stylebox = StyleBoxFlat.new()
+	stylebox.border_color = Color("#ffffff00")
+	stylebox.border_width_bottom = 2
+	stylebox.border_width_top = 2
+	stylebox.border_width_right = 2
+	stylebox.border_width_left = 2
 	add_theme_stylebox_override("normal", stylebox)
 	add_theme_stylebox_override("hover_pressed", stylebox)
 	add_theme_stylebox_override("hover", stylebox)
 	add_theme_stylebox_override("pressed", stylebox)
 
 func _on_cells_change():
+	if button_id == GameEnums.max_roulette_num + 8:
+		stylebox.bg_color = red_color
+	if button_id == GameEnums.max_roulette_num + 9:
+		stylebox.bg_color = black_color
 	if betting_system.get_bet_type(button_id) != GameEnums.bet_types.NUMBER:
 		return
+	
 	if GameManagerGlobal.red_cell_counts[button_id] > 0:
 		print("Changing color of field: ", button_id, " to red")
-		set_color(red_color)
+		stylebox.bg_color = red_color
 	elif GameManagerGlobal.black_cell_counts[button_id] > 0:
 		print("Changing color of field: ", button_id, " to black")
-		set_color(black_color)
+		stylebox.bg_color = black_color
 	else:
 		print("Changing color of field: ", button_id, " to gree")
-		set_color(base_color)
+		stylebox.bg_color = base_color
 
 func _ready() -> void:
+	make_stylebox()
 	add_theme_font_size_override("font_size", betting_system.get_font_size(button_id))
 	$ChipAbove.texture = load("res://assets/textures/chips/chip_black.png")
 	$ChipAbove.visible = false
 	$BetValueLabel.visible = false
-	set_color(base_color)
+	
+	stylebox.bg_color = base_color
 	GameManagerGlobal.signal_change_amount_bet.connect(_on_change_amount)
 	GameManagerGlobal.signal_commit_cell_change_finished.connect(_on_cells_change)
 
 func _on_change_amount(id : int, value : int):
 	if (id != button_id and id != betting_system.all_buttons_id):
-		#print("Caught from button: " + str(button_id) + ", returning")
 		return
-	#print("Caught from button: " + str(button_id) + ", doing")
 	$ChipAbove.texture = betting_system.get_chip_texture(value)
 	amount_bet = value
-	if amount_bet == 0:
-		$ChipAbove.visible = false
-	else:
+	if amount_bet != 0:
 		$ChipAbove.visible = true
-	_on_mouse_entered()
+		_on_mouse_entered()
+	else:
+		$ChipAbove.visible = false
+		_on_mouse_exited()
 
 func _on_button_down() -> void:
 	GameManagerGlobal.signal_placed_bet.emit(button_id)
 
 func _on_mouse_entered() -> void:
-	print("Showing value on: ", button_id)
 	if amount_bet > 0:
+		print("Showing value on: ", button_id)
+		$BetValueLabel.text = "[bgcolor=dim_gray]" + str(amount_bet) + "[/bgcolor]"
+		$BetValueLabel.z_index = 100
 		$BetValueLabel.visible = true
 
 
